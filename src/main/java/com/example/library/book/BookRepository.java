ff96ab55c8002bbb717f482e68d8d0b98c0a16fe
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-01-14 11:00:44 +0100

    feat: add dev profile test data initilizer

--- src/main/java/com/example/library/book/BookRepository.java
+++ src/main/java/com/example/library/book/BookRepository.java
@@ -2,7 +2,12 @@ package com.example.library.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    Optional<Book> findByTitle(String title);
 }
---
+++ src/main/java/com/example/library/config/DevDataInitializer.java
@@ -0,0 +1,95 @@
+package com.example.library.config;
+
+import com.example.library.book.Book;
+import com.example.library.book.BookRepository;
+import com.example.library.loan.Loan;
+import com.example.library.loan.LoanRepository;
+import com.example.library.user.User;
+import com.example.library.user.UserRepository;
+import com.example.library.user.constants.Role;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Profile("dev")
+public class DevDataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final BookRepository bookRepository;
+    private final LoanRepository loanRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+        List<User> users = List.of(
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin123"))
+                        .role(Role.ADMIN)
+                        .build(),
+                User.builder()
+                        .username("user1")
+                        .password(passwordEncoder.encode("pass1"))
+                        .role(Role.USER)
+                        .build(),
+                User.builder()
+                        .username("user2")
+                        .password(passwordEncoder.encode("pass2"))
+                        .role(Role.USER)
+                        .build()
+        );
+
+        List<Book> books = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            int availableCopies = i < 2 ? 0 : 1;
+            int copiesOnLoan = availableCopies == 0 ? 1 : 0;
+            Book book = Book.builder()
+                    .title("Title" + i)
+                    .author("Author" + i)
+                    .availableCopies(availableCopies)
+                    .copiesOnLoan(copiesOnLoan)
+                    .build();
+            books.add(book);
+        }
+
+        userRepository.saveAll(users);
+        bookRepository.saveAll(books);
+
+        User user1 = userRepository.findByUsername("user1")
+                .orElseThrow(EntityNotFoundException::new);
+        Book book1 = bookRepository.findByTitle("Title0")
+                .orElseThrow(EntityNotFoundException::new);
+
+        User user2 = userRepository.findByUsername("user2")
+                .orElseThrow(EntityNotFoundException::new);
+        Book book2 = bookRepository.findByTitle("Title1")
+                .orElseThrow(EntityNotFoundException::new);
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        List<Loan> loans = List.of(
+                Loan.builder()
+                        .user(user1)
+                        .book(book1)
+                        .borrowDate(currentDateTime.minusMinutes(5).minusDays(15))
+                        .returnDate(currentDateTime.minusMinutes(5).minusDays(15).plusMonths(1))
+                        .build(),
+                Loan.builder()
+                        .user(user2)
+                        .book(book2)
+                        .borrowDate(currentDateTime.minusMinutes(15))
+                        .returnDate(currentDateTime.minusMinutes(15).plusMonths(1))
+                        .build()
+        );
+
+        loanRepository.saveAll(loans);
+    }
+}
