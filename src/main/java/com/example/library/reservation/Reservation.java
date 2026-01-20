e76c427853511ffd7f09a03258467ea979fe3978
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-01-20 16:01:31 +0100

    feat: add reservation model

---
+++ src/main/java/com/example/library/reservation/Reservation.java
@@ -0,0 +1,40 @@
+package com.example.library.reservation;
+
+import com.example.library.book.Book;
+import com.example.library.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Book book;
+}
